4b313ccb28ff961cbe0830ded1ae4250404de615
mic_anghileri <micheleanghileri@gmail.com>
2020-09-02 12:17:18 +0200

    search

--- js/script.js
+++ js/script.js
@@ -10,5 +10,36 @@
 // Voto
 
 $(document).ready(function(){
+    $('#cerca').click(function(){
+        var query = $('#query').val()
 
+        $.ajax(
+            {
+                "url":'https://api.themoviedb.org/3/search/movie',
+                "method":'GET',
+                "data": {
+                    api_key:'b71909719ea322aa41b4b92813e721e5',
+                    language:'it-IT',
+                    query: query
+            },
+
+            "success": function(r){
+                for (var i = 0; i < r.results.length; i++){
+                    if(r.results[i].title.includes(query) || r.results[i].original_title.includes(query)){
+                        var source = $("#entry-template").html();
+                        var template = Handlebars.compile(source);
+                        var context = r.results[i];
+                        var html = template(context);
+                        // stampo risultati
+                        $('#risultati').append(html);
+                    } else {
+                        console.log('Nessun risultato')
+                    }
+                }
+            },
+            "error": function(){
+                alert('Errore');
+            }
+        })
+    })
 });
